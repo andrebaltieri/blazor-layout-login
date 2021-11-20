8445eefc1f12f544c9072cad6566c7936cbb0b5f
Andre Baltieri <andrebaltieri@hotmail.com>
2021-11-20 17:32:40 -0300

    Finalizado demo

---
+++ CustomAuthenticationStateProvider.cs
@@ -0,0 +1,20 @@
+﻿namespace BlazorLogin;
+
+using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components.Authorization;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        var identity = new ClaimsIdentity(new[]
+        {
+            new Claim(ClaimTypes.Name, "andrebaltieri"),
+            new Claim(ClaimTypes.Role, "admin"),
+        }, "apiauth");
+
+        var user = new ClaimsPrincipal(identity);
+        return Task.FromResult(new AuthenticationState(user));
+    }
+}
